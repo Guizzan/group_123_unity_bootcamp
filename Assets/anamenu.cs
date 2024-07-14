284436680329b6ce76117ef467a84966f004dd2f
Zexa05 <atakan005555@gmail.com>
2024-07-14 23:55:17 +0300

    update
    update

---
+++ Assets/anamenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class anamenu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+    public void QuitButton()
+    {
+        Application.Quit(); 
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
