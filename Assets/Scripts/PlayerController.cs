f51a22c036541a7570578c8af047f64eb9302855
Zexa05 <atakan005555@gmail.com>
2024-07-22 04:54:17 +0300

    büyük&küçük harf çakışması oldu diye kod bozulmuş onu düzelttim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class Hareket : MonoBehaviour
 {
+
     CharacterController controller;
     Vector3 velocity;
     bool isGrounded;
@@ -18,33 +19,39 @@ public class Hareket : MonoBehaviour
     public float gravity;
 
     public LayerMask mask;
-    { controller = GetComponent<CharacterController>();
-
+        private void Start()
+    {
+        controller = GetComponent<CharacterController>(); 
     }
-
-
-
 private void Update()
 {
     #region Movement
     float horizontal = Input.GetAxis("Horizontal");
     float vertical = Input.GetAxis("Vertical");
-    Vector3 move = Transform.right * horizontal + Transform.forward * vertical;
-    Controller.Move(move * Speed * Time.deltaTime);
+
+    Vector3 move = transform.right * horizontal + transform.forward * vertical;
+    controller.Move(move * speed * Time.deltaTime);
     #endregion
 
     #region jump
-    if(Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+    if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
     {
-        //Jump
+        velocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
     }
-    #endregion Gravity
-    IsGrounded = Physics.CheckSphere(ground.position, distance, mask);
+        #endregion
+        #region Gravity
+        isGrounded = Physics.CheckSphere(ground.position, distance, mask);
 
-    if(isGrounded && velocity)
+    if(isGrounded && velocity.y < 0)
+    {
+        velocity.y = 0f;
+    }
+
+    velocity.y += gravity * Time.deltaTime;
+    controller.Move(velocity * Time.deltaTime);
+     
 
     #endregion
 
-}
-
-} 
+    }
+ }
